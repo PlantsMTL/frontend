f2f861bb955becdce6c612da554dad95fc441c12
Mario Camelo <mariuo@gmail.com>
2023-04-24 15:36:23 -0400

    :lipstick: feat: Adding defaut colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import './assets/styles/custom.scss';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <h1>Hello PlantsMTL</h1>
+    <>
+      <h1>Hello PlantsMTL</h1>
+      <h1>Sign in</h1>
+      <h2>Plants Lists</h2>
+      <h3>Username</h3>
+      <h3>Password</h3>
+    </>
+
   );
 }
 
